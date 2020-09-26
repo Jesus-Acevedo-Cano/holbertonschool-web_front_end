f4f471cc9788eba676d76eca866b60c7f4f02ba8
Jesus-Acevedo-Cano <jeacevedocano@gmail.com>
2020-09-26 00:32:26 -0500

    fix

--- 0x0C-Javascript_advanced/14-wikipedia.js
+++ 0x0C-Javascript_advanced/14-wikipedia.js
@@ -11,11 +11,4 @@ function queryWikipedia(callback) {
     url.send();
     url.onload = () => callback(url.response.query.pages["21721040"].extract);
 }
-function queryWikipedia(callback) {
-    let url = new XMLHttpRequest();
-    url.open("GET", "https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro&explaintext&redirects=1&titles=Stack%20Overflow&origin=*");
-    url.responseType = 'json';
-    url.send();
-    url.onload = () => callback(url.response.query.pages["21721040"].extract);
-}
 queryWikipedia(createElement);
